818bdd65dfc3eba4aa195a4908c43d266e9a32ba
wugoudan <wulok@outlook.com>
2018-10-29 18:01:14 +0800

    修复部分依赖库appcompat重复依赖

---
+++ app/src/main/java/com/goudanbase/compile/TestApp.kt
@@ -0,0 +1,14 @@
+package com.goudanbase.compile
+
+import com.goudanbase.lib.BaseApplication
+
+/**
+ *Conpany:
+ *Auth:goudan
+ *Date:18-10-29 下午5:25
+ *Mail:wulog@outlook.com
+ *Effect:
+ */
+class TestApp: BaseApplication() {
+
+}
